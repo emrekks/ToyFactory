5cbaa9958d86e560bad2d0194f71a5ccd1347023
emrekks <emrekks32@gmail.com>
2023-05-07 16:14:15 +0300

    Test

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,12 @@
+<<<<<<< HEAD
+=======
+using System.Collections;
+>>>>>>> parent of ef2ec1c (Update)
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+<<<<<<< HEAD
     [Header("Movement Settings")]
     [SerializeField, Tooltip("Movement speed of the character")]
     private float moveSpeed = 6.0f;
@@ -10,10 +15,16 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField, Tooltip("Jump height of the character")]
     private float jumpSpeed = 8.0f;
+=======
+    [Header("Movement Settings")] [SerializeField]
+    private float moveSpeed = 6f;
+
+>>>>>>> parent of ef2ec1c (Update)
     [SerializeField] private float jumpSpeed = 8f;
     [SerializeField] private float gravity = 20f;
     [SerializeField] private float lowJumpMultiplier = 1.2f;
 
+<<<<<<< HEAD
     [SerializeField, Tooltip("Gravity force")]
     private float gravity = 20.0f;
     [Header("Climb Settings")] [SerializeField]
@@ -24,12 +35,18 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Slide Settings")]
     [SerializeField, Tooltip("Duration of the slide in seconds")]
+=======
+    [Header("Climb Settings")] [SerializeField]
+    private float climbSpeed = 2f;
+
+>>>>>>> parent of ef2ec1c (Update)
     [Header("Slide Settings")] [SerializeField]
     private float slideDuration = 1f;
 
-    [SerializeField, Tooltip("Multiplier for the slide speed")]
-    private float slideSpeedMultiplier = 2f;
+    [SerializeField] private float slideSpeedMultiplier = 2f;
+    [SerializeField] private float slideCooldown = 1f;
 
+<<<<<<< HEAD
     [SerializeField, Tooltip("Cooldown time for the slide in seconds")]
     private float slideCooldown = 1f;
     [SerializeField] private float slideSpeedMultiplier = 2f;
@@ -38,13 +55,14 @@ public class PlayerMovement : MonoBehaviour
     private bool isJumping = false;
     private float currentJumpSpeed;
     private float verticalVelocity;
+=======
+>>>>>>> parent of ef2ec1c (Update)
     private CharacterController controller;
-
-    private bool isSliding = false;
-    private float slideTimer = 0f;
-    private float slideCooldownTimer = 0f;
     private Vector2 defaultColliderSize;
+<<<<<<< HEAD
     private Vector3 slideDirection;
+=======
+>>>>>>> parent of ef2ec1c (Update)
     private float verticalVelocity;
     private float currentJumpSpeed;
     private float slideTimer;
@@ -58,19 +76,26 @@ public class PlayerMovement : MonoBehaviour
 
     void Start()
     {
+<<<<<<< HEAD
         controller = GetComponent<CharacterController>(); 
+=======
+>>>>>>> parent of ef2ec1c (Update)
         controller = GetComponent<CharacterController>();
         defaultColliderSize = controller.bounds.size;
     }
 
     void Update()
     {
+<<<<<<< HEAD
         if (controller.isGrounded)
+=======
+>>>>>>> parent of ef2ec1c (Update)
         HandleMovement();
         HandleJumping();
         HandleSliding();
         HandleClimbing();
     }
+<<<<<<< HEAD
 
     void HandleMovement()
     {
@@ -141,7 +166,11 @@ public class PlayerMovement : MonoBehaviour
         {
             verticalVelocity -= gravity * Time.deltaTime;
         }
+=======
+>>>>>>> parent of ef2ec1c (Update)
 
+    void HandleMovement()
+    {
         if (!isSliding)
         {
             horizontalInput = Input.GetAxis("Horizontal");
@@ -155,10 +184,59 @@ public class PlayerMovement : MonoBehaviour
         {
             Flip();
         }
+<<<<<<< HEAD
         
         controller.Move(new Vector3(horizontalInput, verticalVelocity, 0) * moveSpeed * Time.deltaTime);
     }
 
+=======
+
+        controller.Move(new Vector3(horizontalInput, verticalVelocity, 0) * moveSpeed * Time.deltaTime);
+    }
+
+    void HandleJumping()
+    {
+        if (!controller.isGrounded)
+        {
+            if (Input.GetButtonUp("Jump"))
+            {
+                isJumping = false;
+
+                if (verticalVelocity > 0)
+                {
+                    verticalVelocity /= lowJumpMultiplier;
+                }
+            }
+
+            if (!isClimbing)
+            {
+                verticalVelocity -= gravity * Time.deltaTime;
+            }
+
+            return; // Early return
+        }
+
+        verticalVelocity = 0;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+            currentJumpSpeed = jumpSpeed;
+        }
+
+        if (isJumping)
+        {
+            verticalVelocity = currentJumpSpeed;
+            currentJumpSpeed -= gravity * Time.deltaTime;
+
+            if (currentJumpSpeed < 0)
+            {
+                isJumping = false;
+            }
+        }
+    }
+
+>>>>>>> parent of ef2ec1c (Update)
     void HandleSliding()
     {
         if (Input.GetKeyDown(KeyCode.LeftControl) && !isSliding && slideCooldownTimer <= 0f)
@@ -167,15 +245,21 @@ public class PlayerMovement : MonoBehaviour
             controller.center = new Vector3(controller.center.x, controller.center.y / 2f, 0);
             slideDirection = transform.forward;
         }
+<<<<<<< HEAD
         
+=======
+>>>>>>> parent of ef2ec1c (Update)
 
         if (isSliding)
         {
             slideTimer -= Time.deltaTime;
 
+<<<<<<< HEAD
             float slideProgress = 1f - slideTimer / slideDuration;
             float slideSpeed = Mathf.Lerp(slideSpeedMultiplier, 0f, slideProgress); 
             controller.Move(slideDirection * slideSpeed * Time.deltaTime); 
+=======
+>>>>>>> parent of ef2ec1c (Update)
             var slideProgress = 1f - slideTimer / slideDuration;
             var slideSpeed = Mathf.Lerp(slideSpeedMultiplier, 0f, slideProgress);
             controller.Move(slideDirection * slideSpeed * Time.deltaTime);
@@ -185,11 +269,35 @@ public class PlayerMovement : MonoBehaviour
                 isSliding = false;
                 slideCooldownTimer = slideCooldown;
                 controller.height *= 2f;
+<<<<<<< HEAD
                 controller.center = new Vector3(controller.center.x, controller.center.y * 2f,0);
+=======
+>>>>>>> parent of ef2ec1c (Update)
                 controller.center = new Vector3(controller.center.x, controller.center.y * 2f, 0);
             }
         }
         slideCooldownTimer -= Time.deltaTime;
+        }
+    }
+
+    void HandleClimbing()
+    {
+        if ((controller.collisionFlags & CollisionFlags.CollidedSides) != 0)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                isClimbing = true;
+                controller.Move(new Vector3(-1f, climbSpeed, 0f) * moveSpeed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                isClimbing = true;
+                controller.Move(new Vector3(1f, climbSpeed, 0f) * moveSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            isClimbing = false;
         }
     }
 
